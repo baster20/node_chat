7da6c116e884d36c807d1c12d6ffde3c2f88e1b9
Oleh <ltvxtyrjj@gmail.com>
2015-03-02 09:25:40 +0200

    Go go go

---
+++ server.js
@@ -0,0 +1,35 @@
+//mongod --dbpath=/data --port 27017
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+//var db_function = require('db_helper');
+// Connection URL
+var db = mongoose.connect('mongodb://localhost:27017/node_chat');
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+
+
+app.get('/', function(req, res){
+    res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({ name: 'Zildjian' });
+//kitty.save(function (err) {
+//    if (err) // ...
+//        console.log('meow');
+//});
+
+
