019119e6d47512010f76b300584ac3cfd6f5181f
Oleh <ltvxtyrjj@gmail.com>
2015-03-18 23:26:42 +0200

    Add modules

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 //mongod --dbpath=/data --port 27017
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var winston = require('winston');
+var app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    winston = require('winston'),
+    bodyParser = require("body-parser"),
+    _ = require("underscore");
 
 var config = require('./libs/config'); // Используемая конфигурация
 var db = require('./libs/mongoose'); // Файл работы с базой MongoDB
+
+app.use(bodyParser.json());
 
 http.listen(3000, function(){
     winston.info('listening on *:3000');
@@ -19,6 +23,12 @@ http.listen(3000, function(){
 // Connection URL
 io.on('connection', function(socket){
     console.log('a user connected');
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
